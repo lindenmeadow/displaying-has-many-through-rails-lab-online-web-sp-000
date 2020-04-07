226257ff0524358f9682dc7b12169e4dd5c62d2a
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-07 18:37:59 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,16 @@
 class Appointment < ApplicationRecord
+  belongs_to :doctor
+  belongs_to :patient
+
+  def scheduled_at
+    self.appointment_datetime.strftime("%B %d, %Y at %k:%M")
+  end
+
+  def doctor_name
+    self.doctor.name
+  end
+
+  def patient_name
+    self.patient.name
+  end
 end
