11a7682d01e314b3c880297d0ccb288421bc2b24
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-07 18:54:45 +0000

    Automatically backed up by Learn

--- db/migrate/20200407185426_create_appointments.rb
+++
@@ -1,9 +0,0 @@
-class CreateAppointments < ActiveRecord::Migration[5.0]
-  def change
-    create_table :appointments do |t|
-      t.string :appointment_dat
-
-      t.timestamps
-    end
-  end
-end
