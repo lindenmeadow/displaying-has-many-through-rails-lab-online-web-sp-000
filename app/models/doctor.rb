5d06f59c63041e71ed82704291765ecf35499f40
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-07 18:50:19 +0000

    Automatically backed up by Learn

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,8 +1,2 @@
-class Doctor < ActiveRecord::Base
-  has_many :appointments
-  has_many :patients, through: :appointments
-
-  def appointment_times
-    self.appointments.sort_by { |a| a.appointment_datetime }
-  end
+class Doctor < ApplicationRecord
 end
